53a678927aa1e106be402f08ab4dabda709e4a98
salbowic <s.albowicz@gmail.com>
2024-08-08 17:22:04 +0200

    modified:   main.py 	modified:   trackers/__init__.py 	new file:   trackers/__pycache__/__init__.cpython-311.pyc 	new file:   trackers/__pycache__/tracker.cpython-311.pyc 	modified:   trackers/tracker.py 	modified:   utils/__pycache__/__init__.cpython-311.pyc 	modified:   utils/__pycache__/video_utils.cpython-311.pyc

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from utils import save_video, read_video
+from trackers import Tracker
 
 def main():
     video_frames = read_video('input_videos/08fd33_4.mp4')
+    
+    tracker = Tracker('models/best.pt')
+    
+    tracks = tracker.get_object_tracks(video_frames)
     
     save_video(video_frames, 'output_videos/output_video.avi')
     
--- trackers/tracker.py
+++ trackers/tracker.py
@@ -14,10 +14,28 @@ class Tracker:
         detections = []
         for i in range(0,len(frames), batch_size):
             detections_batch = self.model.predict(frames[i:i+batch_size], conf=0.1)
-            detections += detections_batch     
+            detections += detections_batch
+            break   
         return detections
     
     def get_object_tracks(self, frames):
         
         detections = self.detect_frames(frames)
+        
+        for frame_num, detection in enumerate(detections):
+            cls_names = detection.names
+            cls_names_inv = {v:k for k,v in cls_names.items()}
+            
+            # Convert to supervision detection format
+            detection_supervision = sv.Detections.from_ultralytics(detection)
+            
+            # Convert goalkeepers to players objects
+            for object_ind, class_id in enumerate(detection_supervision.class_id):
+                if cls_names[class_id] == "goalkeeper":
+                    detection_supervision.class_id[object_ind] = cls_names_inv["player"]
+            
+            # Track objects
+            detection_with_tracks = self.tracker.update_with_detections(detection_supervision)
+            
+            print(detection_with_tracks)
         
