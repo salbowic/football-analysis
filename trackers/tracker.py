0455cb61cf16a3cf429104e31a1661fd2b5d185c
salbowic <s.albowicz@gmail.com>
2024-08-08 17:36:00 +0200

    modified:   trackers/tracker.py

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -22,6 +22,12 @@ class Tracker:
         
         detections = self.detect_frames(frames)
         
+        tracks = {
+            "players":[],
+            "referees":[],
+            "ball":[]
+        }
+        
         for frame_num, detection in enumerate(detections):
             cls_names = detection.names
             cls_names_inv = {v:k for k,v in cls_names.items()}
@@ -37,5 +43,27 @@ class Tracker:
             # Track objects
             detection_with_tracks = self.tracker.update_with_detections(detection_supervision)
             
-            print(detection_with_tracks)
+            tracks["players"].append({})
+            tracks["referees"].append({})
+            tracks["ball"].append({})
+            
+            for frame_detection in detection_with_tracks:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+                track_id = frame_detection[4]
+                
+                if cls_id == cls_names_inv['player']:
+                    tracks["players"][frame_num][track_id] = {"bbox":bbox}
+                
+                if cls_id == cls_names_inv['referee']:
+                    tracks["referees"][frame_num][track_id] = {"bbox":bbox}
+                
+            for frame_detection in detection_supervision:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+                
+                if cls_id == cls_names_inv["ball"]:
+                    tracks["ball"][frame_num][1] = {"bbox":bbox}
+            
+        return tracks
         
