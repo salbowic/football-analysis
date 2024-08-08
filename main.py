c602e22293c86d584e1766535157e4d453c93a6e
salbowic <s.albowicz@gmail.com>
2024-08-08 15:14:40 +0200

    modified:   main.py 	new file:   output_videos/output_video.avi 	modified:   utils/__pycache__/video_utils.cpython-311.pyc 	modified:   utils/video_utils.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils import save_video, read_video
 def main():
     video_frames = read_video('input_videos/08fd33_4.mp4')
     
-    save_video(video_frames, 'output_videos/output_video.mp4')
+    save_video(video_frames, 'output_videos/output_video.avi')
     
 if __name__ == "__main__":
     main()
