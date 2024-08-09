0bf0469760180f4eb0ee17b6e1fec4758ca15d30
salbowic <s.albowicz@gmail.com>
2024-08-09 18:11:18 +0200

    add speed and distance estimator 	modified:   main.py 	modified:   output_videos/output_video.avi 	new file:   speed_and_distance_estimator/__init__.py 	new file:   speed_and_distance_estimator/__pycache__/__init__.cpython-311.pyc 	new file:   speed_and_distance_estimator/__pycache__/speed_and_distance_estimator.cpython-311.pyc 	new file:   speed_and_distance_estimator/speed_and_distance_estimator.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from ball_possesion import assign_ball_possesion
 from player_ball_assigner import PlayerBallAssigner
 from camera_movement_estimator import CameraMovementEstimator
 from view_transformer import ViewTransformer
-
+from speed_and_distance_estimator import SpeedAndDistance_Estimator
 
 def main():
     video_frames = read_video('input_videos/08fd33_4.mp4')
@@ -34,6 +34,10 @@ def main():
     # Interpolate ball postions
     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
     
+    # Speed and distance estimator
+    speed_and_distance_estimator = SpeedAndDistance_Estimator()
+    speed_and_distance_estimator.add_speed_and_distance_to_tracks(tracks)
+    
     # Assign Player Teams
     team_assigner = TeamAssigner()
     tracks = assign_player_teams(tracks, video_frames, team_assigner)
@@ -49,6 +53,9 @@ def main():
     
     ## Draw Camera movement
     output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames, camera_movement_per_frame)
+    
+    ## Draw players speed and distance
+    speed_and_distance_estimator.draw_speed_and_distance(output_video_frames, tracks)
     
     save_video(output_video_frames, 'output_videos/output_video.avi')
     
