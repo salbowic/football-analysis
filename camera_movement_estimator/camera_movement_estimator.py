c3e0b54e4d9b24747edd520a82a6126976f369ef
salbowic <s.albowicz@gmail.com>
2024-08-10 11:42:59 +0200

    modified:   camera_movement_estimator/camera_movement_estimator.py

--- camera_movement_estimator/camera_movement_estimator.py
+++ camera_movement_estimator/camera_movement_estimator.py
@@ -43,7 +43,7 @@ class CameraMovementEstimator():
     
     
     def get_camera_movement(self, frames, read_from_stub=False, stub_path = None):
-        #Read the stub
+        # Read the stub
         if read_from_stub and stub_path is not None and os.path.exists(stub_path):
             with open(stub_path, 'rb') as f:
                 return pickle.load(f)
